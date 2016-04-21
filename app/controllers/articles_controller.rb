83af313cbc4485f4ff1800e065b2218d8280ce6c
Guillaume Vetter <guillaume.vetter@hotmail.com>
2016-04-21 15:17:42 +0200

    Show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,22 @@
 class ArticlesController < ApplicationController
 
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
 	
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(articles_params)
+ 
+		@article.save
+		redirect_to @article
 	end
-
+	
+	private
+	
+	def articles_params
+		params.require(:article).permit(:title, :text)
+	end
 end
